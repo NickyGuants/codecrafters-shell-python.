2e039a9744ac849b49c6401cad2f7d0b5e37b64f
Nicholas Guantai <nicholasguantai528@gmail.com>
2024-12-29 12:09:40 +0300

    implement exit

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
 import sys
 
+
 def main():
     while True:
         sys.stdout.write("$ ")
         # Wait for user input
         user_input = input()
+        match user_input:
+            case 'exit 0':
+                exit_shell()
+            case _:
+                print(f"{user_input}: command not found")
 
-        print(f"{user_input}: command not found")
+
+def exit_shell():
+    sys.exit(0)
 
 
 if __name__ == "__main__":
