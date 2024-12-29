34b0eecc4a0219296d998c4832a93b6762efbde3
Nicholas Guantai <nicholasguantai528@gmail.com>
2024-12-29 12:21:17 +0300

    implement echo command

--- app/main.py
+++ app/main.py
@@ -6,15 +6,20 @@ def main():
         sys.stdout.write("$ ")
         # Wait for user input
         user_input = input()
-        match user_input:
-            case 'exit 0':
-                exit_shell()
+        # differentiate command from arguments
+        command = user_input.split()[0]
+        args = user_input.split()[1:]
+        match command:
+            case 'exit':
+                exit_shell(args[0])
+            case 'echo':
+                print(f"{' '.join(args)}")
             case _:
                 print(f"{user_input}: command not found")
 
 
-def exit_shell():
-    sys.exit(0)
+def exit_shell(arg):
+    sys.exit(int(arg))
 
 
 if __name__ == "__main__":
